8c6dedee58a71dcf78492ab4ad20c34741a0a76f
jhpleimann <j.plmnn@gmail.com>
2019-09-10 15:30:55 -0500

    Version 1.01

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private uint nuggets = 6;
+
+        public double Price { get; set;
+        }
+        public uint Calories {get; set;
+        }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() {};
+                for(int i = 0; i < nuggets; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 354;
+        }
+
+        public void AddNugget()
+        {
+            this.nuggets++;
+            this.Calories += 59;
+            this.Price += .25;
+        }
+    }
+}
---
+++ Menu/Entrees/SteakosaurusBurger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class SteakosaurusBurger
+    {
+        private bool bun = true;
+        private bool lettuce = true;
+        private bool tomato = true;
+        private bool onion = true;
+        private bool pickle = true;
+        private bool ketchup = true;
+        private bool mustard = true;
+        private bool mayo = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie" };
+                if (bun) ingredients.Add("Whole Wheat Bun");
+                if (pickle) ingredients.Add("Pickle");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                return ingredients;
+            }
+        }
+
+        public SteakosaurusBurger()
+        {
+            this.Price = 5.15;
+            this.Calories = 621;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+        public void HoldPickle()
+        {
+            this.pickle = false;
+        }
+        public void HoldKetchup()
+        {
+            this.ketchup = false;
+        }
+        public void HoldMustard()
+        {
+            this.mustard = false;
+        }
+    }
+}
